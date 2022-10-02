36b851c14074995e925485ce3caba72060ade647
wangxiao <wxiao1002@yeah.net>
2022-10-02 22:31:52 +0800

    docs(*): rebuid

--- src/main/java/com/github/baoan/WebSocketApplication.java
+++ src/main/java/com/github/baoan/WebSocketApplication.java
@@ -1,9 +1,17 @@
 package com.github.baoan;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * web Socket 启动类
  * @author wang xiao
  * @date 2022/5/11
  */
+@SpringBootApplication
 public class WebSocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebSocketApplication.class, args);
+    }
 }
